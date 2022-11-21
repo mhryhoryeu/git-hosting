a05e16ee2516a1fab772f49cf882d57ffcd22a1a
Maksim Hryhoryeu <maksim.hryhoryeu@gmail.com>
2022-11-21 16:01:50 +0000

    multiply operation has been added

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(add(10, 10));
+        System.out.println(multiply(10, 10));
     }
 
     public static double add(double a, double b) {
         return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
     }
 }
