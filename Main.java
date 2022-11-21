636fdf3269cf4c8674bad2ee5c240b610e14425c
Maksim Hryhoryeu <maksim.hryhoryeu@gmail.com>
2022-11-21 16:03:19 +0000

    divide operation has been added

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(add(10, 10));
         System.out.println(multiply(10, 10));
+        System.out.println(divide(10, 10));
     }
 
     public static double add(double a, double b) {
@@ -10,5 +11,9 @@ public class Main {
 
     public static double multiply(double a, double b) {
         return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
     }
 }
