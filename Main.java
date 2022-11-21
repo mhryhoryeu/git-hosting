ac5d724cebf1bf5a346cf9b8a9a6688b97c34d3f
Maksim Hryhoryeu <maksim.hryhoryeu@gmail.com>
2022-11-21 16:04:59 +0000

    subtract operation has been added

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println(add(10, 10));
         System.out.println(multiply(10, 10));
         System.out.println(divide(10, 10));
+        System.out.println(subtract(10, 10));
     }
 
     public static double add(double a, double b) {
@@ -15,5 +16,9 @@ public class Main {
 
     public static double divide(double a, double b) {
         return a / b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
     }
 }
